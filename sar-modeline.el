e4f4f70eb21a7f87883cdeda7e17930340e3dd47
Scott Runnels <srunnels@gmail.com>
2026-08-16 22:24:47 -0400

    bugfix

--- sar-modeline.el
+++ sar-modeline.el
@@ -5,7 +5,7 @@
 ;; Author: Scott Runnels <srunnels@gmail.com>
 ;; URL: https://github.com/srunnels/dotfiles
 ;; Version: 0.0.0
-;; Package-Requires: ((emacs "30.1") (s "1.13.0") ())
+;; Package-Requires: ((emacs "30.1") (s "1.13.0"))
 
 
 ;;; Commentary:
